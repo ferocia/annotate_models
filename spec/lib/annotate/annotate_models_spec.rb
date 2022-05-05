800670de81b42709a67eb0fb8715d38d8fb88e7f
Lovro Bikic <lovro.bikic@infinum.hr>
2022-05-05 16:06:46 +0200

    Handle missing expression
    Signed-off-by: Lovro Bikic <lovro.bikic@infinum.hr>

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -784,6 +784,7 @@ describe AnnotateModels do
                     [
                       mock_check_constraint('alive', 'age < 150'),
                       mock_check_constraint('must_be_adult', 'age >= 18'),
+                      mock_check_constraint('missing_expression', nil),
                       mock_check_constraint('multiline_test', <<~SQL)
                         CASE
                           WHEN (age >= 18) THEN (age <= 21)
@@ -804,9 +805,10 @@ describe AnnotateModels do
                       #
                       # Check Constraints
                       #
-                      #  alive           (age < 150)
-                      #  multiline_test  (CASE WHEN (age >= 18) THEN (age <= 21) ELSE true END)
-                      #  must_be_adult   (age >= 18)
+                      #  alive               (age < 150)
+                      #  missing_expression
+                      #  multiline_test      (CASE WHEN (age >= 18) THEN (age <= 21) ELSE true END)
+                      #  must_be_adult       (age >= 18)
                       #
                     EOS
                   end
@@ -1585,6 +1587,7 @@ describe AnnotateModels do
                   let :check_constraints do
                     [
                       mock_check_constraint('min_name_length', 'LENGTH(name) > 2'),
+                      mock_check_constraint('missing_expression', nil),
                       mock_check_constraint('multiline_test', <<~SQL)
                         CASE
                           WHEN (age >= 18) THEN (age <= 21)
@@ -1610,6 +1613,7 @@ describe AnnotateModels do
                       # ### Check Constraints
                       #
                       # * `min_name_length`: `(LENGTH(name) > 2)`
+                      # * `missing_expression`
                       # * `multiline_test`: `(CASE WHEN (age >= 18) THEN (age <= 21) ELSE true END)`
                       #
                     EOS
